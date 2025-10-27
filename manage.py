0506c521b275fa905196051472270c0a5e8bb94f
AnuvA26 <142830611+AnuvA26@users.noreply.github.com>
2025-10-28 04:56:02 +0600

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -7,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SkillSwap.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CORE.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
