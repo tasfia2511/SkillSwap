0d0f67c8d05c295d0f90adc84782191d16e400a0
AnuvA26 <142830611+AnuvA26@users.noreply.github.com>
2025-10-28 04:53:19 +0600

    Delete app/admin.py

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Skill, CanTeach, CanLearn
-
-admin.site.register(Skill)
-admin.site.register(CanTeach)
-admin.site.register(CanLearn)
-
